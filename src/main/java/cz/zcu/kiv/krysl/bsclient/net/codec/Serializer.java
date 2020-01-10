ced1e987279f2b13de0b5b0f87209511bada9b85
Miroslav Krýsl <mkrysl@protonmail.com>
2020-01-10 11:02:49 +0100

    Refactor Serializer and Deserialize
    Make them work with string encoding instead of raw bytes

---
+++ src/main/java/cz/zcu/kiv/krysl/bsclient/net/codec/Serializer.java
@@ -0,0 +1,36 @@
+package cz.zcu.kiv.krysl.bsclient.net.codec;
+
+import cz.zcu.kiv.krysl.bsclient.net.connection.ISerializer;
+import cz.zcu.kiv.krysl.bsclient.net.message.Message;
+import cz.zcu.kiv.krysl.bsclient.util.Helper;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.Charset;
+
+public class Serializer implements ISerializer<Message> {
+
+    private static final String CHARS_TO_ESCAPE = String.valueOf(CodecConstants.MESSAGE_END);
+    private static final String MESSAGE_END = String.valueOf(CodecConstants.MESSAGE_END);
+
+    private Charset encoding;
+
+    public Serializer(Charset encoding) {
+        this.encoding = encoding;
+    }
+
+    @Override
+    public byte[] serialize(Message message) {
+        ByteArrayOutputStream serialized = new ByteArrayOutputStream();
+
+        MessageSerializer messageSerializer = new MessageSerializer(message);
+        String messageString = messageSerializer.serialize();
+
+        // escape message end char
+        messageString = Helper.escapeChars(messageString, CHARS_TO_ESCAPE, CodecConstants.ESCAPE);
+
+        serialized.writeBytes(encoding.encode(messageString).array());
+        serialized.writeBytes(encoding.encode(MESSAGE_END).array());
+
+        return serialized.toByteArray();
+    }
+}
