bdff6ef117a0cdbf587a13b71702c18e5de48c5e
Miroslav Krýsl <mkrysl@protonmail.com>
2020-01-10 11:17:54 +0100

    Add Codec class with constants

---
+++ src/main/java/cz/zcu/kiv/krysl/bsclient/net/codec/Codec.java
@@ -0,0 +1,10 @@
+package cz.zcu.kiv.krysl.bsclient.net.codec;
+
+public class Codec {
+    public static final char PAYLOAD_START = ':';
+    public static final char PAYLOAD_ITEM_SEPARATOR = ';';
+    public static final char MESSAGE_END = '\n';
+    public static final char ESCAPE = '\\';
+    public static final int MAX_MESSAGE_LENGTH = 1024;
+    public static final int CHAR_BUFFER_LENGTH = 2048;
+}
